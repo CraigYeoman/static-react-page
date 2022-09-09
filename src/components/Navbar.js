703f26d1ef794c6ae77420f4930c8882eacf106c
CraigYeoman <yeomancraig@gmail.com>
2022-09-09 07:15:59 -0500

    Initial setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Navbar() {
+    return (
+        <nav>
+            <img src="../images/logo192.png" className="nav--icon" alt="logo"/>
+        </nav>
+    )
+}
+
+export default Navbar;
