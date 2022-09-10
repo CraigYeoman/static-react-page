ad9cf7af029bc77b952487e7099b26563dd352f4
CraigYeoman <yeomancraig@gmail.com>
2022-09-10 07:39:33 -0500

    Setting up nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,13 @@
 import React from "react"
+import "../style.css"
+import logo from "../images/logo192.png"
 
 function Navbar() {
     return (
         <nav>
-            <img src="../images/logo192.png" className="nav--icon" alt="logo"/>
+            <img src={logo} className="nav--icon" alt="logo"/>
+            <div className="nav--title">ReactFacts</div>
+            <div className="nav--heading">React Course - Project 1 </div>
         </nav>
     )
 }
