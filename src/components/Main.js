b2e6caf72a9be615c8598d06d385c912c0d31f69
CraigYeoman <yeomancraig@gmail.com>
2022-09-11 15:05:23 -0500

    Finishing project

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,18 @@
 import React from "react"
+import "../style.css"
 
 function Main() {
     return (
-        <div>Main Component</div>
+        <div className="main--container">
+            <div className="main--header">Fun facts about React</div>
+            <ul className="main--list">
+                <li>Was first released in 2013</li>
+                <li>Was orginally created by Jordan Walke</li>
+                <li>Has well over 100k stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </div>
     )
 }
 
